5e90b70880ed17848f1dbecfeef8096a50a70b81
lerucom <lerucom@gmail.com>
2019-05-03 21:15:03 +0300

    Загрузка данных для страницы товаров

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import './main.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {createStore, applyMiddleware} from 'redux'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'  // для удобного debug
+import thunk from 'redux-thunk' // для ассинхронных запросов
 import {syncHistoryWithStore} from 'react-router-redux'
 import {Router, Route, browserHistory} from 'react-router'
 import {Provider} from 'react-redux'
@@ -22,7 +22,7 @@ const history = syncHistoryWithStore(browserHistory, store);
 ReactDOM.render(
     <Provider store={store}>
         <Router history={history}>
-            <Route component={Layout}>
+            <Route component={Layout}> // стандарная обертка вокруг любой страницы, содержит место для content и sidebar
                 <Route path='/' component={Parts} />
             </Route>
         </Router>
