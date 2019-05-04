1025980e45e0ff4e443592ef7315d6b1056533fd
lerucom <lerucom@gmail.com>
2019-05-04 22:50:03 +0300

    Подготовка данных для страницы товара

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import './main.css'
+import './main.css';
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {createStore, applyMiddleware} from 'redux'
-import {composeWithDevTools} from 'redux-devtools-extension'  // для удобного debug
-import thunk from 'redux-thunk' // для ассинхронных запросов
-import {syncHistoryWithStore} from 'react-router-redux'
-import {Router, Route, browserHistory} from 'react-router'
-import {Provider} from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';  // для удобного debug
+import thunk from 'redux-thunk'; // для ассинхронных запросов
+import {syncHistoryWithStore} from 'react-router-redux';
+import {Router, Route, browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
 
-import reducers from 'reducers'
-import Layout from 'containers/layout'
-import Parts from 'containers/parts'
+import reducers from 'reducers';
+import Layout from 'containers/layout';
+import Parts from 'containers/parts';
+import Part from 'containers/part';
 
 const store = createStore(reducers, composeWithDevTools(
     applyMiddleware(thunk)
@@ -25,6 +26,7 @@ ReactDOM.render(
             <Route component={Layout}> // стандарная обертка вокруг любой страницы, содержит место для content и sidebar
                 <Route path='/' component={Parts} />
             </Route>
+            <Route path='parts/:id' component={Part} />
         </Router>
     </Provider>,
     document.getElementById('root')
